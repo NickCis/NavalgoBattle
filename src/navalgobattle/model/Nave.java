5bafcee126331d0cb0b64a4a0229601910f158ac
Nicolas Cisco <n.cis_92@hotmail.com>
2013-06-03 04:32:46 -0300

    Se arregla build.xml. Se agrega Clase nave y su respectivo test

--- src/navalgobattle/model/Nave.java
+++ src/navalgobattle/model/Nave.java
@@ -12,27 +12,119 @@ import navalgobattle.model.Disparo;
  */
 public class Nave {
 	protected int tam;
-	protected int direccion;
-	protected ArrayList<Posicion> posVidas;
-	protected NavalgoBattle juego;
+	protected int direccion; //-> Guarda en que direccion se mueve
+	protected ArrayList<Posicion> posVidas; //-> Lista de posiciones con la vida respectiva de cada poisicion
+	protected int xMax;
+	protected int yMax;
+	//protected NavalgoBattle juego;
 
 	public Nave(int xMax, int yMax){
+		this.xMax = xMax;
+		this.yMax = yMax;
+		this.posVidas = new ArrayList<Posicion>();
 	}
 
+	public Nave(int xMax, int yMax, int x, int y){
+		this(xMax, yMax);
+		this.setPosicion(x, y);
+	}
+
+	public Nave(int xMax, int yMax, int x, int y, int direccion){
+		this(xMax, yMax, x, y);
+		this.setDireccion(direccion);
+	}
+	
+
+	/** Setea la posicion.
+	 * @param int x: posicion x
+	 * @param int y: posicion y
+	 */
+
+	public void setPosicion(int x, int y){
+		Posicion estaPos = new Posicion(x, y, 1);
+		this.posVidas.add(estaPos);
+	}
+
+	/** Setea la direccion a cual se movera.
+	 * @param int dir, se va a usar como flags se usaran los ultimos 4 bytes, 
+	 * 1 1 1 1
+	 * | | | |_ Indicara si se mueve en x ( & 1)
+	 * | | |___ Indica signo de x, 1 menos, 0 mas ( & 2 )
+	 * | |_____ Indicia si se mueve en y ( & 4)
+	 * |_______ Indica signo de y, 1 menos 0 mas. ( & 8 )
+	 */
+	public void setDireccion(int dir){
+		this.direccion = dir;
+	}
 	/** Mueve la nave.
 	 * 1) Efectua movimiento de la nave, teniendo en cuenta si rebota o no contra los costados
 	 * 2) Actualiza las posiciones de su arrayList.
-	 * @return boolean, verdadero si la nave se movio y tiene vida, falso si la nave esta muerta.
 	 */
-	public boolean mover(){
+	
+	public void mover(){
+		if(!this.puedoMover())
+			this.espejarDireccion();
+
+		for(Posicion pos : this.posVidas){
+			// TODO: este laburo se podria pasar a la posicion
+			int newX = pos.getX();
+			int newY = pos.getY();
+			if((this.direccion & 1) != 0)
+				newX += ( ((this.direccion & 2) != 0)? -1: 1 );
+			if((this.direccion & 4) != 0)
+				newY += ( ((this.direccion & 8) != 0)? -1: 1 );
+			pos.setXY(newX, newY);
+		}
+	}
+
+	public boolean estoyEnPosicion(int x, int y){
+		Posicion estaPos = new Posicion(x, y, 0);
+		for(Posicion pos : this.posVidas){
+			if(pos.equals(estaPos))
+				return true;
+		}
+
 		return false;
+	}
+
+	/** Dice si se puede mover a la siguiente posicion.
+	 * @return verdadero si puede, falso si no.
+	 */
+	protected boolean puedoMover(){
+		for(Posicion pos : this.posVidas){
+			// TODO: este laburo se podria pasar a la posicion
+			if((this.direccion & 1) != 0){
+				int newX = pos.getX() + ( ((this.direccion & 2)!= 0)? -1: 1 );
+				if(newX < 0 || newX > this.xMax)
+					return false;
+			}
+			if((this.direccion & 4) != 0){
+				int newY = pos.getY() + ( ((this.direccion & 8) != 0)? -1: 1 );
+				if(newY < 0 || newY > this.yMax)
+					return false;
+			}
+		}
+		return true;
+	}
+
+	/** Espeja direccion de movimiento.
+	 * Basicamente lo que hace es alternar los dos flags de signo de la direccion
+	 */
+	protected void espejarDireccion(){
+		this.direccion ^= 10;
 	}
 
 	/** Esta viva.
 	 * @return boolean devuelve si la nave esta viva.
 	 */
 	public boolean estaViva(){
-		return false;
+		boolean estaViva = false;
+		for(Posicion pos : this.posVidas){
+			if(pos.getVida() > 0)
+				estaViva = true;
+		}
+
+		return estaViva;
 	}
 
 	/** Danar a una nave.
@@ -74,12 +166,10 @@ class Posicion {
 
 	@Override
 	public boolean equals(Object obj) {
-		Posicion posicion = (Posicion) obj;
-		return posicion.x == this.x && posicion.y == this.y;
-	}
+		if(obj == null) return false;
+		if(obj.getClass() != getClass()) return false;
 
-	@Override
-	public String toString() {
-		return x + ", " + y;
+		Posicion pos = (Posicion)obj;
+		return pos.getX() == this.getX() && pos.getY() == this.getY();
 	}
 }
---
+++ test/navalgobattle/modelo/NaveTest.java
@@ -0,0 +1,61 @@
+package navalgobattle.model;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NaveTest extends TestCase{
+	@Before
+	public void setUp(){
+	}
+	
+	@Test
+	public void testCrearNave() {
+		Nave nave = new Nave(5, 5, 0, 0, 1);
+		this.assertEquals(true, nave.estoyEnPosicion(0, 0));
+	}
+
+	@Test
+	public void testMoverX(){
+		Nave nave = new Nave(5, 5, 0, 0, 1);
+		nave.mover();
+		this.assertEquals(true, nave.estoyEnPosicion(1, 0));
+	}
+
+	@Test
+	public void testRebotarX(){
+		Nave nave = new Nave(5, 5, 5, 0, 1);
+		nave.mover();
+
+		this.assertEquals(true, nave.estoyEnPosicion(4, 0));
+	}
+	@Test
+	public void testMoverY(){
+		Nave nave = new Nave(5, 5, 0, 0, 4);
+		nave.mover();
+		this.assertEquals(true, nave.estoyEnPosicion(0, 1));
+	}
+
+	@Test
+	public void testRebotarY(){
+		Nave nave = new Nave(5, 5, 0, 5, 4);
+		nave.mover();
+
+		this.assertEquals(true, nave.estoyEnPosicion(0, 4));
+	}
+	@Test
+	public void testMoverXY(){
+		Nave nave = new Nave(5, 5, 0, 0, 5);
+		nave.mover();
+		this.assertEquals(true, nave.estoyEnPosicion(1, 1));
+	}
+
+	@Test
+	public void testRebotarXY(){
+		Nave nave = new Nave(5, 5, 5, 5, 5);
+		nave.mover();
+
+		this.assertEquals(true, nave.estoyEnPosicion(4, 4));
+	}
+}
