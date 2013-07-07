0e7e105bd47995d9918d1d299a5389a35e54b544
Nicolas Cisco <n.cis_92@hotmail.com>
2013-07-07 19:01:23 -0300

    Se agregan UML de view y controller

--- src/navalgobattle/controller/CalculadoraDePixeles.java
+++ src/navalgobattle/controller/CalculadoraDePixeles.java
@@ -22,34 +22,34 @@ public class CalculadoraDePixeles {
 
 	/** Devuelve el ancho en pixel de un cuadrado (una posicion).
 	 */
-	protected int squareWidth(){
+	public int squareWidth(){
 		return (int) Math.round(this.width / (this.juego.getMaximaPosicion().getX()+1));
 	}
 	/** Devuelve el alto en pixel de un cuadrado (una posicion).
 	 */
-	protected int squareHeight(){
+	public int squareHeight(){
 		return (int) Math.round(this.height / (this.juego.getMaximaPosicion().getY()+1));
 	}
 	/** Convierte pixeles del eje x en posiciones.
 	 */
-	protected int x2pos(int x){
+	public int x2pos(int x){
 		return (int) Math.round(x * (this.juego.getMaximaPosicion().getX()+1)/ this.width);
 	}
 	/** Convierte posiciones en pixeles del eje x (devuelve el pixel en el medio del cuadrado).
 	 */
-	protected int pos2x(int xPos){
+	public int pos2x(int xPos){
 		return (int) Math.round(xPos * this.width / (this.juego.getMaximaPosicion().getX()+1) +  this.squareWidth()/2);
 	}
 
 	/** Convierte pixeles del eje y en posiciones.
 	 */
-	protected int y2pos(int y){
+	public int y2pos(int y){
 		return (int) Math.round(y * (this.juego.getMaximaPosicion().getY()+1)/ this.height);
 	}
 
 	/** Convierte posiciones en pixeles del eje y (devuelve el pixel en el medio del cuadrado).
 	 */
-	protected int pos2y(int yPos){
+	public int pos2y(int yPos){
 		return (int) Math.round(yPos * this.height/ (this.juego.getMaximaPosicion().getY()+1)+this.squareHeight()/2);
 	}
 }
